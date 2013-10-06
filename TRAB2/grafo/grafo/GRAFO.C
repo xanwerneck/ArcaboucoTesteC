14142fc5590348faeb3bbaec17d0c6cd756fa37a
Alexandre Werneck <xanwerneck@gmail.com>
2013-10-06 02:31:42 -0300

    novas correcoes

--- TRAB2/grafo/grafo/GRAFO.C
+++ TRAB2/grafo/grafo/GRAFO.C
@@ -86,7 +86,8 @@ typedef struct GRA_tagGrafo {
 
 } GRA_tpGrafo ;
 
-/************* Funções encapsuladas no módulo *************************/
+
+/************ Funções encapsuladas no módulo *************************/
 
 void GRA_ExcluirdeVertices(GRA_tppGrafo pGrafo , tpVerticeGrafo * pVertice);
  
