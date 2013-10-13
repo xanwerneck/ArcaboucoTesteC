4b0c9060f1e034b45bfd61852b13a225b65932b8
Ferlief <ferlief@gmail.com>
2013-10-12 23:08:34 -0300

    Alteracao na documentacao do LISTA.C e LISTA.H do modulo GRAFO

--- TRAB2/grafo/grafo/LISTA.C
+++ TRAB2/grafo/grafo/LISTA.C
@@ -1,7 +1,8 @@
 /***************************************************************************
-*  $MCI Módulo de implementação: LIS  Lista duplamente encadeada
 *
-*  Arquivo gerado:              LISTA.c
+*  $MCD Módulo de definição: LIS  Lista duplamente encadeada
+*
+*  Arquivo gerado:              LISTA.H
 *  Letras identificadoras:      LIS
 *
 *  Nome da base de software:    Arcabouço para a automação de testes de programas redigidos em C
@@ -9,14 +10,13 @@
 *
 *  Projeto: INF 1301 / 1628 Automatização dos testes de módulos C
 *  Gestor:  LES/DI/PUC-Rio
-*  Autores: avs
+*  Autores: aw - Alexandre Werneck
+*           fr - Fernanda Camelo Ribeiro
+*           vo - Vinicius de Luiz de Oliveira
 *
 *  $HA Histórico de evolução:
 *     Versão  Autor    Data     Observações
-*     4       avs   01/fev/2006 criar linguagem script simbólica
-*     3       avs   08/dez/2004 uniformização dos exemplos
-*     2       avs   07/jul/2003 unificação de todos os módulos em um só projeto
-*     1       avs   16/abr/2003 início desenvolvimento
+*     1       afv   01/out/2013 início desenvolvimento
 *
 ***************************************************************************/
 
@@ -28,7 +28,7 @@
 
 
 #define LISTA_OWN
-#include "LISTA.h"
+#include "LISTA.H"
 #undef LISTA_OWN
 
 
@@ -266,9 +266,10 @@
 
    } /* Fim função: LIS  &Inserir elemento após */
 
-/*
-LIS &ChecaOrigemNulo
-*/
+/***************************************************************************
+*
+*  Função: LIS  &Checa se a origem é nula
+*  ****/
 LIS_tpCondRet LIS_ChecaOrigemNulo(LIS_tppLista pLista)
 {
 
@@ -278,10 +279,11 @@ LIS_tpCondRet LIS_ChecaOrigemNulo(LIS_tppLista pLista)
 
 	if(pLista->pOrigemLista == NULL){
 		return LIS_CondRetListaVazia;
-	}
+	} /* if */
+
 	return LIS_CondRetOK;
 
-}
+}  /* Fim função: LIS  &Checa se a origem é nula */
 
 
 
@@ -586,10 +588,6 @@ LIS_tpCondRet LIS_ChecaOrigemNulo(LIS_tppLista pLista)
       pLista->numElem   = 0 ;
 
    } /* Fim função: LIS  -Limpar a cabeça da lista */
-
-int LIS_NumElem(LIS_tppLista pLista){
-	return pLista->numElem;
-}
 
 /********** Fim do módulo de implementação: LIS  Lista duplamente encadeada **********/
 
