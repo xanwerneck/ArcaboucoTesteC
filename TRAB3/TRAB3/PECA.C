5e57dffb9d9b44f74ef50cca29af11265a1395db
Alexandre Werneck <xanwerneck@gmail.com>
2013-11-03 20:32:05 -0200

    Alteracoes na documentacao do modulo PECA

--- TRAB3/TRAB3/PECA.C
+++ TRAB3/TRAB3/PECA.C
@@ -1,22 +1,20 @@
 /***************************************************************************
 *
-*  $MCD Módulo de definição: PEC  Peca do tabuleiro
+*  $MCD Módulo de definição: PEC  Peça do tabuleiro
 *
-*  Arquivo gerado:              PECA.C
+*  Arquivo gerado:              PECA.H
 *  Letras identificadoras:      PEC
 *
-*  Nome da base de software:    Arcabouço para a automação de testes de programas redigidos em C
-*  Arquivo da base de software: D:\AUTOTEST\PROJETOS\SIMPLES.BSW
-*
-*  Projeto: INF 1301 Automatização dos testes de módulos C
-*  Gestor:  LES/DI/PUC-Rio
-*  Autores: aw - Alexandre Werneck
-*           fr - Fernanda Camelo Ribeiro
-*			vo - Vinicius de Luiz de Oliveira
+*  Projeto: INF 1301 - Verificador de Xeque-Mate
+*  Gestor:  Flavio Bevilacqua
+*  Autores: afv:  aw - Alexandre Werneck
+*                 fr - Fernanda C Ribeiro
+*			      vo - Vinicius de Luiz de Oliveira
 *
 *  $HA Histórico de evolução:
 *     Versão  Autor    Data     Observações
-*     1       afv   19/out/2013 início desenvolvimento
+*     Y       afv   xx/xx/2013  finalização do desenvolvimento do modulo
+*     1       afv   24/out/2013 início do desenvolvimento do módulo
 *
 ***************************************************************************/
 
@@ -37,7 +35,7 @@
 
 /***********************************************************************
 *
-*  $TC Tipo de dados: PEC Descritor do elemento Peca
+*  $TC Tipo de dados: PEC Descritor do tipo de peca
 *
 *
 ***********************************************************************/
@@ -45,16 +43,24 @@
    typedef struct PEC_tagElemPeca {
 
 		char Nome[MAX_NOME];
-		
+		/* String com o nome da peça */
+
 		int diagonal;
-		
+		/* Se peça pode ir para diagonal 
+		(1 para sim, 0 caso contrário) */
+
 		int reta;
-		
+		/* Se peça pode ir para horizontal e vertical
+		 (1 para sim, 0 caso contrário) */
+
 		int qtde;
+		/* Quantidade casas que a peça pode andar */
 		  
    } PEC_tpElemPeca ;
 
 /***** Protótipos das funções encapsuladas no módulo *****/
+
+
 
 /*****  Código das funções exportadas pelo módulo  *****/
 
@@ -73,7 +79,7 @@ PEC_tpCondRet PEC_CriarPeca(PEC_tppPeca * pPeca, int Diag , int Reta , int Qtde 
 	if(mPeca == NULL)
 	{
 		return PEC_CondRetFaltouMemoria ;
-	}	
+	} /* if */
 	
 	mPeca->diagonal = Diag;
 	mPeca->reta     = Reta;
@@ -81,6 +87,11 @@ PEC_tpCondRet PEC_CriarPeca(PEC_tppPeca * pPeca, int Diag , int Reta , int Qtde 
 	strcpy(mPeca->Nome , Nome);
 	
 	(*pPeca) = (PEC_tpElemPeca *) malloc(sizeof(PEC_tppPeca));
+
+	if(pPeca == NULL)
+	{
+		return PEC_CondRetFaltouMemoria ;
+	} /* if */
 	
 	(*pPeca) = mPeca;
 	
@@ -88,34 +99,48 @@ PEC_tpCondRet PEC_CriarPeca(PEC_tppPeca * pPeca, int Diag , int Reta , int Qtde 
 	
 } /* Fim função: PEC &Criar Peca */
 
+/***************************************************************************
+*
+*  Função: PEC  &Obter nome
+*  ****/
+
 PEC_tpCondRet PEC_ObterNome (PEC_tppPeca pPeca ,  void ** NomePeca)
 {
 	if(pPeca == NULL)
 	{
 		return PEC_CondRetPecaNula ;
-	}
+	} /* if */
 	
 	strcpy((char *)NomePeca , pPeca->Nome);
 
 	
 	return PEC_CondRetOK;
-}
+} /* Fim função: PEC &Obter nome */
+
+/***************************************************************************
+*
+*  Função: PEC  &Obter dados do tipo peça
+*  ****/
 
 PEC_tpCondRet PEC_ObterDadosTipoPeca (PEC_tppPeca pPeca , void ** Nome, int * Diag , int * Reta , int * Qtde)
 {
 	if(pPeca == NULL)
 	{
 		return PEC_CondRetPecaNula ;
-	}
+	} /* if */
+
 	strcpy((char *)Nome , pPeca->Nome);
-	*Diag = pPeca->diagonal ;
+	*Diag = pPeca->diagonal;
 	*Reta = pPeca->reta;
 	*Qtde = pPeca->qtde;
 
 	return PEC_CondRetOK;
 
-}
+} /* Fim função: PEC &Obter dados do tipo peça */
 
 
-/********** Fim do módulo de implementação: LIS  Lista duplamente encadeada **********/
+/*****  Código das funções encapsuladas pelo módulo  *****/
+
+
+/********** Fim do módulo de implementação: PEC  Peça do tabuleiro **********/
 
